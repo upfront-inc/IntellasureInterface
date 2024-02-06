6f942cd19764c6f5e0b2f5d0a59d620434c73fa7
omar jandali <omarjandali93@gmail.com>
2024-02-06 13:46:52 -0800

    Omar Jandali. Added a theme context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useTheme } from './Contexts/ThemeContext'; 
 
 function App() {
+  const { theme, toggleTheme } = useTheme(); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <p>Intellasure</p>
+      <div className='toggle-button' onClick={toggleTheme}><p>Toggle Theme</p></div>
     </div>
   );
 }
