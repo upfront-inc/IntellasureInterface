f06e5ecfc5a1630b2ac3030858153a5b8be70276
omar jandali <omarjandali93@gmail.com>
2024-02-09 16:50:34 -0800

    Omar Jandali. Shows filter when your clicks on facility filter

--- src/Components/FilterComponent.js
+++ src/Components/FilterComponent.js
@@ -5,24 +5,65 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown, faX } from '@fortawesome/free-solid-svg-icons';
 import { useTheme } from '../Contexts/ThemeContext';
 import { useApp } from '../Contexts/AppContext';
+import { useSidebar } from '../Contexts/SidebarContext';
 
 const FilterComponent = () => {
 
   const { theme } = useTheme()
-  const { toggleTableFilter } = useApp()
+  const { sidebarPosition } = useSidebar()
+  const { toggleTableFilter,
+    filterFacility, toggleFilterFacility,
+    filterColumns, toggleFilterColumns  } = useApp()
+
+  const displayFilterFacility = () => {
+    return(
+      <div className={`facilities-filter-container-${sidebarPosition}`}>
+        <div>
+          <p>All Facilities</p>
+        </div>
+        <div>
+          <p>Axis</p>
+        </div>
+        <div>
+          <p>Beachside</p>
+        </div>
+        <div>
+          <p>Affinity</p>
+        </div>
+      </div>
+    )
+  }
+
+  const displayFilterColumns = () => {
+    return(
+      <div>
+        <p>bye</p>
+      </div>
+    )
+  }
 
   return (
     <div className={`filter-bar-${theme}`}>
       <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
         <p>Filters: </p>
-        <div className={`filter-container-${theme}`}>
+        <div onClick={() => {toggleFilterFacility()}} className={`filter-container-${theme}`}>
           <p>Facilities</p>
           <FontAwesomeIcon className='filter-icon' icon={faChevronDown} />
         </div>
-        <div className={`filter-container-${theme}`}>
+        {
+          filterFacility
+            ? displayFilterFacility()
+            : null
+        }
+        <div onClick={() => {toggleFilterColumns()}} className={`filter-container-${theme}`}>
           <p>Columns</p>
           <FontAwesomeIcon className='filter-icon' icon={faChevronDown} />
         </div>
+        {
+          filterColumns
+            ? displayFilterColumns()
+            : null
+        }
       </div>
       <div onClick={() => {toggleTableFilter()}}>
         <FontAwesomeIcon icon={faX} className='search-icon'/>
