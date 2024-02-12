d4ed13e72c6c750e22d3a0f34116b98d636dbc8e
omar jandali <omarjandali93@gmail.com>
2024-02-12 09:54:01 -0800

    Omar Jandali. Added the missing records tab

---
+++ src/Components/MissingTableComponent.js
@@ -0,0 +1,288 @@
+import React from 'react'
+import { useTheme } from '../Contexts/ThemeContext'
+
+const MissingTableComponent = () => {
+
+  const { theme } = useTheme()
+
+  return (
+    <div className='table-parent'>
+      <table className='table-section'>
+        <thead className={`table-header-${theme}`}>
+          <tr>
+            <th className='table-header-text'>
+              Prefix
+            </th>
+            <th className='table-header-text'>
+              Policy
+            </th>
+            <th className='table-header-text'>
+              Name
+            </th>
+            <th className='table-header-text'>
+              Insurance
+            </th>
+            <th className='table-header-text'>
+              Network
+            </th>
+            <th className='table-header-text'>
+              Facility
+            </th>
+            <th className='table-header-text'>
+              Residential
+            </th>
+            <th className='table-header-text'>
+              Detox
+            </th>
+            <th className='table-header-text'>
+              Total Charges
+            </th>
+            <th className='table-header-text'>
+              Total Paid
+            </th>
+            <th className='table-header-text'>
+              Payout %
+            </th>
+            <th className='table-header-text'>
+              Admission
+            </th>
+            <th className='table-header-text'>
+              Admission %
+            </th>
+            <th className='table-header-text'>
+              Details 
+            </th>
+          </tr>
+        </thead>
+        <tbody className={`table-body-${theme}`}>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+          <tr className='table-content-row' style={{textAlign: 'center', marginTop: '6px', marginBottom: '6px'}}>
+            <td>SVD</td>
+            <td>SVD2134872y743</td>
+            <td>John Doe</td>
+            <td>AETNA</td>
+            <td>In-Network</td>
+            <td>AXIS</td>
+            <td>12 Days / 2 Visits</td>
+            <td>18 Days / 1 Visit</td>
+            <td>$18,324</td>
+            <td>$10,2232</td>
+            <td>81%</td>
+            <td>Likely</td>
+            <td>78%</td>
+            <td>Open/Close</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default MissingTableComponent
--- src/Screens/MissingScreen.js
+++ src/Screens/MissingScreen.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import '../Css/Content.css'
+import '../Css/Table.css'
+import { useTheme } from '../Contexts/ThemeContext'
+import { useApp } from '../Contexts/AppContext';
+import TopBarComponent from '../Components/TopBarComponent';
+import FilterComponent from '../Components/FilterComponent';
+import MissingTableComponent from '../Components/MissingTableComponent';
 
 const MissingScreen = () => {
+
+  const { theme } = useTheme()
+  const { tableFilter, toggleTableFilter } = useApp()
+
   return (
-    <div>
-      <p>Missing</p>
+    <div className={`content-container-${theme}`}>
+      <TopBarComponent />
+      {
+        tableFilter 
+          ? <FilterComponent/>
+          : null 
+      }
+      <div className='table-container'>
+        <MissingTableComponent />
+      </div>
     </div>
   )
 }
